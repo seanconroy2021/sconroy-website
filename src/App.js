48a1504570e024fc21560b135c9eaa8b657d5bbf
Sean Conroy <sconroy@seanmacbook.lan>
2024-07-06 17:10:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-import Homepage from "./pages/homepage";
+// import Homepage from "./pages/homepage";
 // import About from "./pages/about";
 // import Projects from "./pages/projects";
 import Articles from "./pages/articles";
 import ReadArticle from "./pages/readArticle";
 // import Contact from "./pages/contact";
-import Notfound from "./pages/404";
+// import Notfound from "./pages/404";
 import Maintenance from "./pages/underMaintenance";
 
 import { TRACKING_ID } from "./data/tracking";
